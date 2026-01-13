554ac864eb3894b65c9c6e2c7700dd47564cd6b2
LAVANYA B Y <lavanyalavanya0555@gmail.com>
2026-01-13 18:46:29 +0530

    Time: 2 ms (38.30%) | Memory: 12.6 MB (31.17%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+
+        prefix = strs[0]
+
+        for i in range(len(prefix)):
+            for word in strs[1:]:
+                if i >= len(word) or word[i] != prefix[i]:
+                    return prefix[:i]
+
+        return prefix
