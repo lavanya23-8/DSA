f90074bd65dacf4bb1aef74aa937395ee45a3186
LAVANYA B Y <lavanyalavanya0555@gmail.com>
2026-02-20 22:08:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (91.79%) - LeetSync

---
+++ 62-unique-paths/unique-paths.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        dp = [1] * n
+        
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[j] += dp[j-1]
+        
+        return dp[n-1]
