437119c19edb0c03035436ef03011d56d1e469e6
LAVANYA B Y <lavanyalavanya0555@gmail.com>
2026-06-01 18:31:26 +0530

    Time: 626 ms (66.19%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+WITH first_order AS (
+    SELECT *,
+           ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY order_date) AS rn
+    FROM Delivery
+)
+
+SELECT
+    ROUND(
+        100.0 * SUM(CASE
+                        WHEN order_date = customer_pref_delivery_date
+                        THEN 1
+                        ELSE 0
+                    END)
+        / COUNT(*),
+        2
+    ) AS immediate_percentage
+FROM first_order
+WHERE rn = 1;
